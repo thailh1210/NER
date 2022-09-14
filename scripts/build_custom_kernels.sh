c91c4aef48919cd5f01bd1634f75720b9c41026e
thailh1210 <111763695+thailh1210@users.noreply.github.com>
2022-09-14 20:10:58 +0700

    Update build_custom_kernels.sh

--- scripts/build_custom_kernels.sh
+++ scripts/build_custom_kernels.sh
@@ -5,7 +5,7 @@ TF_DIR=$(dirname $TF_ORIGIN)
 TF_INC=$TF_DIR/include
 TF_LIB=$TF_DIR
 
-g++ -std=c++11 -shared srl_kernels.cc -o srl_kernels.so \
+g++ -std=c++14 -shared /content/NER/srl_kernels.cc -o srl_kernels.so \
     -I$TF_INC -I$TF_INC/external/nsync/public \
     -L$TF_LIB -ltensorflow_framework \
     -fPIC -D_GLIBCXX_USE_CXX11_ABI=1 -O2
